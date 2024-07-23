bd875cb27484fdc0412a710bd8e0165aacbeb054
Akshitha1583 <128457535+Akshitha1583@users.noreply.github.com>
2024-07-23 19:00:27 +0530

    day_1
    day_1

---
+++ 1.py
@@ -0,0 +1,6 @@
+name=input()
+age=int(input())
+    if(age>18):
+       print(f"hey,{akshitha}you are eligible for voting")
+    else:
+       print(f"hey,{akshitha}you are not eligible for voting")
---
+++ arithmetic.py
@@ -0,0 +1,15 @@
+a=13
+b=4
+print(a+b)
+
+a=13
+b=4
+print(a-b)
+
+a=13
+b=4
+print(a*b)
+
+a=13
+b=4
+print(a%b)
